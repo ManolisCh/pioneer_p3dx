bb35ebd9b9ae54e96c4f99eb1e2bdaba94857d8d
ManolisCh <manolis.chiou@gmail.com>
2015-01-09 18:01:19 +0000

    finished node

--- laser_noise/src/laser_noise_node.cpp
+++ laser_noise/src/laser_noise_node.cpp
@@ -22,6 +22,7 @@ private:
     sensor_msgs::LaserScan addedNoiseScan_;
 
     void laserReadCallBAck(const sensor_msgs::LaserScan::ConstPtr& msg);
+    double GaussianKernel(double mu,double sigma);
 
 };
 
@@ -34,6 +35,23 @@ void LaserNoise::laserReadCallBAck(const sensor_msgs::LaserScan::ConstPtr& msg)
 
 {
     addedNoiseScan_ = *msg ;
+
+
+    for (int i=0; i < addedNoiseScan_.ranges.size() ; i++)
+
+    {
+    addedNoiseScan_.ranges[i] = addedNoiseScan_.ranges[i] + GaussianKernel(0,2);
+
+    if (addedNoiseScan_.ranges[i] > addedNoiseScan_.range_max)
+       { addedNoiseScan_.ranges[i] = addedNoiseScan_.range_max;}
+
+    else if (addedNoiseScan_.ranges[i] < addedNoiseScan_.range_min)
+       { addedNoiseScan_.ranges[i] = addedNoiseScan_.range_min;}
+    }
+
+
+
+
     addedNoiseScan_.header.stamp = ros::Time::now();
 
     scan_pub_.publish(addedNoiseScan_);
@@ -42,7 +60,20 @@ void LaserNoise::laserReadCallBAck(const sensor_msgs::LaserScan::ConstPtr& msg)
 }
 
 
+// Utility for adding noise
+double LaserNoise::GaussianKernel(double mu,double sigma)
+{
+    // using Box-Muller transform to generate two independent standard normally disbributed normal variables
 
+    double U = (double)rand()/(double)RAND_MAX; // normalized uniform random variable
+    double V = (double)rand()/(double)RAND_MAX; // normalized uniform random variable
+    double X = sqrt(-2.0 * ::log(U)) * cos( 2.0*M_PI * V);
+    //double Y = sqrt(-2.0 * ::log(U)) * sin( 2.0*M_PI * V); // the other indep. normal variable
+    // we'll just use X
+    // scale to our mu and sigma
+    X = sigma * X + mu;
+    return X;
+}
 
 
 int main(int argc, char** argv)
